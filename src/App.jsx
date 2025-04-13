b15b82b14c3db877599cba453fa7546e5e650015
Poornachandra123 <96694008+Poornachandra123@users.noreply.github.com>
2025-04-13 16:45:55 +0530

    updated homepage url and app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import './styles/index.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio">
       <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
         <Navbar />
         <main className="flex-grow">
